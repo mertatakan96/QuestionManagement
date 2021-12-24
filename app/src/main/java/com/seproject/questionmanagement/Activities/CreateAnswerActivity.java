939bac3909e35abdfd607e3bcf9b51711378682c
Mert Atakan Duduoğlu <mert.duduoglu@isik.edu.tr>
2021-12-24 21:08:15 +0300

    questionnaire create
    questionnaire and answer creation working

--- app/src/main/java/com/seproject/questionmanagement/Activities/CreateAnswerActivity.java
+++ app/src/main/java/com/seproject/questionmanagement/Activities/CreateAnswerActivity.java
@@ -2,13 +2,17 @@ package com.seproject.questionmanagement.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.seproject.questionmanagement.Questionnaire;
 import com.seproject.questionmanagement.R;
 
 import java.util.ArrayList;
@@ -18,9 +22,13 @@ public class CreateAnswerActivity extends AppCompatActivity implements View.OnCl
     //RecyclerView recyclerView;
 
     LinearLayout linearLayoutAnswer;
-    Button buttonAddAnswer;
+    Button buttonAddAnswer, buttonSendAnswers;
     TextView questionName;
     ArrayList<String> questionnairesList = new ArrayList<>();
+    ArrayList<String> answerList = new ArrayList<>();
+    private int questionNumber = 0;
+    private int clickCount = 0;
+    //Questionnaire questionnaire = new Questionnaire();
 
 
     @Override
@@ -28,7 +36,7 @@ public class CreateAnswerActivity extends AppCompatActivity implements View.OnCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_answer);
 
-        //recyclerView = findViewById(R.id.recycler_add_answers);
+
 
         linearLayoutAnswer = findViewById(R.id.layout_answer_list);
 
@@ -39,17 +47,50 @@ public class CreateAnswerActivity extends AppCompatActivity implements View.OnCl
 
         buttonAddAnswer = findViewById(R.id.button_add_answer);
 
+        buttonSendAnswers = findViewById(R.id.button_send_answers);
+
         buttonAddAnswer.setOnClickListener(this);
 
-        for (int i=0; i<questionnairesList.size();i++){
-            questionName.setText(questionnairesList.get(i));
-        }
+        buttonSendAnswers.setOnClickListener(this);
+        //System.out.println("Açılış Ekranı: " + questionNumber); 0
+        //updateQuestion(); update ve printi böl
+        //System.out.println("Açılış Ekranı: " + questionNumber);
+        questionName.setText("Miss Click");
+
+
+        /*buttonSendAnswers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //checkIfValidAndRead();
+                updateQuestion();
+                //checkIfValidAndRead();
+
+            }
+        });*/
+
     }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_add_answer:
+                addView();
+                break;
 
-        addView();
+            case R.id.button_send_answers:
+                if (clickCount == 0){
+                    questionName.setText(questionnairesList.get(questionNumber));
+                    clickCount++;
+                }else{
+
+                    //System.out.println("Update Öncesi: " + questionNumber);
+                    updateQuestion();
+                    linearLayoutAnswer.removeAllViews(); // bir sonraki soruya geçerken tüm cevapları temizliyor.
+                    //System.out.println("Update Sonrası: " + questionNumber);
+                }
+
+
+        }
 
     }
 
@@ -60,6 +101,73 @@ public class CreateAnswerActivity extends AppCompatActivity implements View.OnCl
         TextView textView = (TextView) addAnswerView.findViewById(R.id.edit_answer_name);
         ImageView imageClose = (ImageView) addAnswerView.findViewById(R.id.answer_remove);
 
+        imageClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeView(addAnswerView);
+            }
+        });
+
         linearLayoutAnswer.addView(addAnswerView);
+    }
+
+    private void removeView(View v){
+        linearLayoutAnswer.removeView(v);
+    }
+
+
+    private void updateQuestion(){
+       if ((questionnairesList.size()) == (questionNumber)){
+            Intent intentToProfile = new Intent(this,ProfileActivity.class);
+            startActivity(intentToProfile);
+            Toast.makeText(getBaseContext(), "Your Questionnaire Created", Toast.LENGTH_LONG).show();
+        }else{
+           //questionName.setText(questionnairesList.get(questionNumber));
+           //System.out.println("Update Q: " + questionNumber);
+           checkIfValidAndRead();
+           //System.out.println("After Q: " + questionNumber);
+           questionNumber++;
+           if (questionNumber != questionnairesList.size()){
+               questionName.setText(questionnairesList.get(questionNumber));
+           }
+           else {
+               updateQuestion(); //questionNumber setText den önce arttığı için array sınırını aşarsa methoda girip if state'i görsün önce
+           }
+           //questionName.setText(questionnairesList.get(questionNumber));
+           //questionNumber++;
+           //System.out.println(questionnairesList);
+           //System.out.println(answerList);
+           //System.out.println("Soru: " + questionnaire.getQuestion() + " Cevaplar: " + questionnaire.getAnswers());
+       }
+
+    }
+
+    private boolean checkIfValidAndRead(){
+        answerList.clear();
+
+        //Questionnaire questionnaire = new Questionnaire();
+        boolean result = true;
+
+        for (int i=0;i<linearLayoutAnswer.getChildCount();i++){
+
+            View addAnswerView = linearLayoutAnswer.getChildAt(i);
+
+            EditText editTextAnswer = (EditText) addAnswerView.findViewById(R.id.edit_answer_name);
+
+            Questionnaire questionnaire = new Questionnaire();
+
+            if (!editTextAnswer.getText().equals("")){
+                questionnaire.setQuestion(questionnairesList.get(questionNumber)); // sorunun adını al
+                answerList.add(editTextAnswer.getText().toString());
+                questionnaire.setAnswers(answerList);   //sorunun cevaplarını al
+                //System.out.println(questionNumber);
+                System.out.println("Soru: " + questionnaire.getQuestion() + " Cevaplar: " + questionnaire.getAnswers());
+                //answerList.add(editTextAnswer.getText().toString());
+            }else {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
